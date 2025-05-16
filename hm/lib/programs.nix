aaa1f526e7aac9fd65547f098cb20b72cf391b67
Bahar KURT <kurtbahartr@users.noreply.github.com>
2025-05-16 23:27:17 +0300

    hm: Install zsh-nix-shell
    I want zsh to possess nix-shells.

--- hm/lib/programs.nix
+++ hm/lib/programs.nix
@@ -61,6 +61,23 @@
         backend = "fastfetch";
       };
     };
+    # zsh-nix-shell
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      plugins = [
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.8.0";
+            sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+          };
+        }
+      ];
+    };
     # Distrobox config is available only on unstable channel.
     #distrobox = {
     #  enable = true;
