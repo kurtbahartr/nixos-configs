c37bf9aa544afddca68f098ab8ccc995bb0e814a
Bahar KURT <kurtbahartr@users.noreply.github.com>
2025-05-18 19:55:48 +0300

    Install low-level packages related to S.M.A.R.T data

--- lib/lowlevel.nix
+++ lib/lowlevel.nix
@@ -11,10 +11,15 @@
   # KDE Partition Manager
   programs.partition-manager.enable = true;
 
+  # smartd service from smartmontools
+  services.smartd.enable = true;
+
   environment.systemPackages = with pkgs; [
     efibootmgr
     android-tools
     pciutils
+    nvme-cli
+    smartmontools
     inputs.envycontrol.packages.${pkgs.system}.default
     exfat
     (callPackage ../pkgs/odin4.nix { })
