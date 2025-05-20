a9be1be778723ce70cbfce8d87af04b63894801e
Bahar KURT <kurtbahartr@users.noreply.github.com>
2025-05-20 16:55:01 +0300

    flake: Bump Nixpkgs and Home Manager to NixOS 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A simple NixOS flake";
   inputs = {
     # NixOS official package source, using the nixos-24.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     # Flatpak for NixOS
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.6.0";
     # Zen Browser
@@ -12,7 +12,7 @@
     };
     # Home Manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # EnvyControl
