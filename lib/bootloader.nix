2adae118b72937573c063c2bfb50aa33f4a7405b
Bahar KURT <kurtbahartr@users.noreply.github.com>
2025-05-21 11:39:10 +0300

    Switch to Limine bootloader

--- lib/bootloader.nix
+++ lib/bootloader.nix
@@ -1,4 +1,8 @@
 {
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.limine = {
+    enable = true;
+    maxGenerations = 10;
+    style.interface.branding = "Linda's Excalibur";
+  };
   boot.loader.efi.canTouchEfiVariables = true;
 }
