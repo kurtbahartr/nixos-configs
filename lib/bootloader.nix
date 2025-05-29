8c18ab650b0726511aae0c1e96dae33a70499f27
Bahar KURT <kurtbahartr@users.noreply.github.com>
2025-05-29 21:05:06 +0300

    Remove redundant `bootloader.nix`
    It's been superseded with a split bootloader configuration.

--- lib/bootloader.nix
+++
@@ -1,8 +0,0 @@
-{
-  boot.loader.limine = {
-    enable = true;
-    maxGenerations = 10;
-    style.interface.branding = "Linda's Excalibur";
-  };
-  boot.loader.efi.canTouchEfiVariables = true;
-}
